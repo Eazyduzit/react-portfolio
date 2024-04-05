378c80c6731387a543d32874bdacfc6f59658041
Marius Skjellerud <m_skjellerud@hotmail.com>
2024-04-05 13:18:25 +0200

    added conditional for above small screens

--- src/scenes/Navbar.tsx
+++ src/scenes/Navbar.tsx
@@ -12,6 +12,12 @@ const Navbar = ({ selectedPage, setSelectedPage }: Props) => {
     <nav className={`z-40 w-full fixed top-0 py-6`}>
       <div className="flex items-center justify-between mx-auto w-5/6">
         <h4 className="font-playfair text-3xl font-bold">JE</h4>
+        {/* DESKTOP NAV */}
+        {isAboveSmallScreens ? (
+          <div className="flex justify-between gap-16 font-opensans text-sm font-semibold"></div>
+        ) : (
+          <div></div>
+        )}
       </div>
     </nav>
   )
