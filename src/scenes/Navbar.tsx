ae89eb2bf1b7d5faf3535efcb4e14c087d6d51ea
Marius Skjellerud <m_skjellerud@hotmail.com>
2024-04-05 13:07:31 +0200

    added use state for menu toggle and media query for small screens

--- src/scenes/Navbar.tsx
+++ src/scenes/Navbar.tsx
@@ -1,11 +1,20 @@
 import { useState } from "react"
 import AnchorLink from "react-anchor-link-smooth-scroll"
 import useMediaQuery from "../hooks/useMediaQuery"
+import React from "react"
 
 type Props = {}
 
 const Navbar = ({ selectedPage, setSelectedPage }: Props) => {
-  return <div>Navbar</div>
+  const [isMenuToggled, setIsMenuToggled] = useState(false)
+  const isAboveSmallScreens = useMediaQuery("(min-width: 768px)")
+  return (
+    <nav className={`z-40 w-full fixed top-0 py-6`}>
+      <div className="flex items-center justify-between mx-auto w-5/6">
+        <h4 className="font-playfair text-3xl font-bold">JE</h4>
+      </div>
+    </nav>
+  )
 }
 
 export default Navbar
