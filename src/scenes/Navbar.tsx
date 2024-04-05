15aac8ad3a5e8930b93ece8b80caa1d1b9dd5def
Marius Skjellerud <m_skjellerud@hotmail.com>
2024-04-05 12:54:45 +0200

    Created scenes folder and navbar file, and created Navbar component. added imports, set params

---
+++ src/scenes/Navbar.tsx
@@ -0,0 +1,11 @@
+import { useState } from "react"
+import AnchorLink from "react-anchor-link-smooth-scroll"
+import useMediaQuery from "../hooks/useMediaQuery"
+
+type Props = {}
+
+const Navbar = ({ selectedPage, setSelectedPage }: Props) => {
+  return <div>Navbar</div>
+}
+
+export default Navbar
