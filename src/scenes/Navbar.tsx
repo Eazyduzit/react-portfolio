d243dad81652b46bda4fa0921e823d89b5a008d6
Marius Skjellerud <m_skjellerud@hotmail.com>
2024-04-09 11:33:44 +0200

    created Link function using Anchor link with conditional and onclick

--- src/scenes/Navbar.tsx
+++ src/scenes/Navbar.tsx
@@ -5,6 +5,20 @@ import React from "react"
 
 type Props = {}
 
+const Link = ({ page, selectedPage, setSelectedPage }) => {
+  const lowerCasePage = page.toLowerCase()
+  return (
+    <AnchorLink
+      className={`${selectedPage === lowerCasePage ? "text-yellow" : ""}
+    hover:text-yellow transition duration-500`}
+      href={`#${lowerCasePage}`}
+      onClick={() => setSelectedPage(lowerCasePage)}
+    >
+      {page}
+    </AnchorLink>
+  )
+}
+
 const Navbar = ({ selectedPage, setSelectedPage }: Props) => {
   const [isMenuToggled, setIsMenuToggled] = useState(false)
   const isAboveSmallScreens = useMediaQuery("(min-width: 768px)")
