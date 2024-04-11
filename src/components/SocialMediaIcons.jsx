67564c547b4d12f809522c466c5cbaff2aecfeb2
Marius Skjellerud <m_skjellerud@hotmail.com>
2024-04-11 13:45:27 +0200

    added links for linkedin, twitter, facebook, instagram

--- src/components/SocialMediaIcons.jsx
+++ src/components/SocialMediaIcons.jsx
@@ -9,6 +9,30 @@ const SocialMediaIcons = () => {
       >
         <img src="../assets/linkedin.png" alt="linkedin-link" />
       </a>
+      <a
+        className="hover:opacity-50 transition duration-500"
+        href="https://www.twitter.com"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img src="../assets/twitter.png" alt="twitter-link" />
+      </a>
+      <a
+        className="hover:opacity-50 transition duration-500"
+        href="https://www.facebook.com"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img src="../assets/facebook.png" alt="facebook-link" />
+      </a>
+      <a
+        className="hover:opacity-50 transition duration-500"
+        href="https://www.instagram.com"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img src="../assets/instagram.png" alt="instagram-link" />
+      </a>
     </div>
   )
 }
