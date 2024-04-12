6397078c9282b4a18725b4ac9e51f03ceba6929a
Marius Skjellerud <m_skjellerud@hotmail.com>
2024-04-12 17:14:23 +0200

    added imports, mediaquery and section

--- src/scenes/MySkills.jsx
+++ src/scenes/MySkills.jsx
@@ -1,5 +1,10 @@
+import LineGradient from "../components/LineGradient"
+import useMediaQuery from "../hooks/useMediaQuery"
+import { motion } from "framer-motion"
+
 const MySkills = () => {
-  return <div></div>
+  const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)")
+  return <section id="skills" className="pt-10 pb-24"></section>
 }
 
 export default MySkills
