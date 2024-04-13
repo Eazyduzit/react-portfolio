6e37e01d38b74bfe75a90ac767e4f04ad238d4e1
Marius Skjellerud <m_skjellerud@hotmail.com>
2024-04-13 15:16:41 +0200

    added skills and experience section using a motion div

--- src/scenes/MySkills.jsx
+++ src/scenes/MySkills.jsx
@@ -43,6 +43,26 @@ const MySkills = () => {
           )}
         </div>
       </div>
+
+      {/* SKILLS */}
+      <div className="md:flex md:justify-between mt-16 gap-32">
+        {/* EXPERIENCE */}
+        <motion.div
+          className="md:w-1/3 mt-10"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ duration: 0.5 }}
+          variants={{ hidden: { opacity: 0, y: 50 }, visible: { opacity: 1, y: 0 } }}
+        >
+          <div className="relative h-32">
+            <div className="z-10">
+              <p className="font-playfair font-semibold text-5xl">01</p>
+              <p className="font-playfair font-semibold text-5xl">Experience</p>
+            </div>
+          </div>
+        </motion.div>
+      </div>
     </section>
   )
 }
