a83227ae6e990dc9bff4c3d797848623c23e7a2d
Marius Skjellerud <m_skjellerud@hotmail.com>
2024-04-18 12:17:47 +0200

    created contact component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Landing from "./scenes/Landing"
 import MySkills from "./scenes/MySkills"
 import Projects from "./scenes/Projects"
 import Testimonials from "./scenes/Testimonials"
+import Contact from "./scenes/Contact"
 import LineGradient from "./components/LineGradient"
 import { useEffect, useState } from "react"
 import useMediaQuery from "./hooks/useMediaQuery"
@@ -46,6 +47,10 @@ function App() {
       <LineGradient />
       <div className="w-5/6 mx-auto md:h-full">
         <Testimonials />
+      </div>
+      <LineGradient />
+      <div className="w-5/6 mx-auto md:h-full">
+        <Contact />
       </div>
     </div>
   )
---
+++ src/scenes/Contact.jsx
@@ -0,0 +1,12 @@
+import LineGradient from "../components/LineGradient"
+import { motion } from "framer-motion"
+
+const Contact = () => {
+  return (
+    <section>
+      <div>Contact</div>
+    </section>
+  )
+}
+
+export default Contact
